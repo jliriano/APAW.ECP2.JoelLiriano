14f8a4c4e27aba72ec0ec498110611608d5d0ed6
jliriano <jd.liriano@alumnos.upm.es>
2018-10-09 13:18:05 +0200

    remove code smells and fix vulnerability

--- src/main/java/api/Dispatcher.java
+++ src/main/java/api/Dispatcher.java
@@ -4,29 +4,33 @@ import api.exceptions.RequestInvalidException;
 import http.HttpRequest;
 import http.HttpResponse;
 import http.HttpStatus;
+import org.apache.logging.log4j.LogManager;
 
 public class Dispatcher {
 
+    private final String REQUEST_ERROR = "request error: ";
+    private final String METHOD_ERROR = "method error: ";
+
     public void submit(HttpRequest request, HttpResponse response) {
-        String ERROR_MESSAGE = "{'error':'%S'}";
+        String error_message = "{'error':'%S'}";
         try {
             switch (request.getMethod()) {
                 case POST:
-                    throw new RequestInvalidException("method error: " + request.getMethod());
+                    throw new RequestInvalidException(METHOD_ERROR + request.getMethod());
                 case GET:
-                    throw new RequestInvalidException("request error: " + request.getMethod() + ' ' + request.getPath());
+                    throw new RequestInvalidException(REQUEST_ERROR + request.getMethod() + ' ' + request.getPath());
                 case PUT:
-                    throw new RequestInvalidException("request error: " + request.getMethod() + ' ' + request.getPath());
+                    throw new RequestInvalidException(REQUEST_ERROR + request.getMethod() + ' ' + request.getPath());
                 case PATCH:
-                    throw new RequestInvalidException("request error: " + request.getMethod() + ' ' + request.getPath());
+                    throw new RequestInvalidException(REQUEST_ERROR + request.getMethod() + ' ' + request.getPath());
                 case DELETE:
-                    throw new RequestInvalidException("request error: " + request.getMethod() + ' ' + request.getPath());
+                    throw new RequestInvalidException(REQUEST_ERROR + request.getMethod() + ' ' + request.getPath());
                 default:
-                    throw new RequestInvalidException("method error: " + request.getMethod());
+                    throw new RequestInvalidException(METHOD_ERROR + request.getMethod());
             }
         } catch (Exception exception) {  // Unexpected
-            exception.printStackTrace();
-            response.setBody(String.format(ERROR_MESSAGE, exception));
+            LogManager.getLogger().error(exception.getStackTrace());
+            response.setBody(String.format(error_message, exception));
             response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
