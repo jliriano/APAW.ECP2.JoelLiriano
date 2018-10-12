3a351dd8b0b8c2c9421e99a966dd0ec6fbfdfc20
jliriano <jd.liriano@alumnos.upm.es>
2018-10-12 13:59:46 +0200

    added tests to GamesIT - issue #03

--- src/test/java/api/GamesIT.java
+++ src/test/java/api/GamesIT.java
@@ -7,14 +7,16 @@ import api.daos.memory.DaoMemoryFactory;
 import api.dtos.GameDto;
 import api.dtos.PublisherDto;
 import http.Client;
+import http.HttpException;
 import http.HttpRequest;
+import http.HttpStatus;
 import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.apache.logging.log4j.LogManager;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
-
 import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GamesIT {
 
@@ -31,6 +33,54 @@ public class GamesIT {
         LogManager.getLogger().info("publisherId: "+publisherId);
         String gameId = this.createGame("Game 1", publisherId, null, null);
         LogManager.getLogger().info(new Client().submit(this.getPublisher(publisherId)).getBody());
+    }
+
+    @Test
+    void testCreateGamePublisherNotFoundException() {
+        GameDto gameDto = new GameDto("Game Name", "75");
+        HttpRequest request = HttpRequest.builder(PublisherApiController.PUBLISHERS
+                +"/75"+GameApiController.GAMES).body(gameDto).post();
+        HttpException exception = assertThrows(HttpException.class, () -> new Client().submit(request));
+        assertEquals(HttpStatus.NOT_FOUND, exception.getHttpStatus());
+    }
+
+    @Test
+    void testCreateGameInvalidArgumentsException() {
+        String publisherId = this.createPublisher();
+        GameDto gameDto = new GameDto(null,null);
+        HttpRequest request = HttpRequest.builder(PublisherApiController.PUBLISHERS
+                +"/"+publisherId+GameApiController.GAMES).body(gameDto).post();
+        HttpException exception = assertThrows(HttpException.class, () -> new Client().submit(request));
+        assertEquals(HttpStatus.BAD_REQUEST, exception.getHttpStatus());
+    }
+
+    @Test
+    void testCreateGameWithoutName() {
+        String publisherId = this.createPublisher();
+        GameDto gameDto = new GameDto(null,publisherId);
+        HttpRequest request = HttpRequest.builder(PublisherApiController.PUBLISHERS
+                +"/"+publisherId+GameApiController.GAMES).body(null).post();
+        HttpException exception = assertThrows(HttpException.class, () -> new Client().submit(request));
+        assertEquals(HttpStatus.BAD_REQUEST, exception.getHttpStatus());
+    }
+
+    @Test
+    void testCreateGameWithoutPublisherId() {
+        String publisherId = this.createPublisher();
+        GameDto gameDto = new GameDto("Game Name",null);
+        HttpRequest request = HttpRequest.builder(PublisherApiController.PUBLISHERS
+                +"/"+publisherId+GameApiController.GAMES).body(null).post();
+        HttpException exception = assertThrows(HttpException.class, () -> new Client().submit(request));
+        assertEquals(HttpStatus.BAD_REQUEST, exception.getHttpStatus());
+    }
+
+    @Test
+    void testCreateGameWithoutGameDto() {
+        String publisherId = this.createPublisher();
+        HttpRequest request = HttpRequest.builder(PublisherApiController.PUBLISHERS
+                +"/"+publisherId+GameApiController.GAMES).body(null).post();
+        HttpException exception = assertThrows(HttpException.class, () -> new Client().submit(request));
+        assertEquals(HttpStatus.BAD_REQUEST, exception.getHttpStatus());
     }
 
     @Ignore
