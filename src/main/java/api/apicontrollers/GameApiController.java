93fa6c54abc41d443010f6680f3d1d015a554ff3
jliriano <jd.liriano@alumnos.upm.es>
2018-10-12 15:03:35 +0200

    update GameApiController to support PATCH operation, fix Dispatcher statement - issue #04

--- src/main/java/api/apicontrollers/GameApiController.java
+++ src/main/java/api/apicontrollers/GameApiController.java
@@ -6,6 +6,8 @@ import api.dtos.GameDto;
 public class GameApiController extends BasicApiController {
 
     public static final String GAMES = "/games";
+    public static final String ID_ID = "/{id}";
+    public static final String NAME = "/name";
 
     private GameBusinessController gameBusinessController = new GameBusinessController();
 
@@ -14,6 +16,15 @@ public class GameApiController extends BasicApiController {
         this.validate(gameDto.getName(), "GameDto Name");
         this.validate(gameDto.getPublisherId(), "GameDto PublisherId");
         return this.gameBusinessController.create(gameDto);
+    }
+
+    public String update(String publisherId, String gameId, GameDto gameDto) {
+        this.validate(publisherId, "publisherId");
+        this.validate(gameId, "gameId");
+        this.validate(gameDto, "gameDto");
+        gameDto.setId(gameId);
+        gameDto.setPublisherId(publisherId);
+        return this.gameBusinessController.update(gameDto);
     }
 
 }
