66ef666c6db65c4ce66e2a4498ae58ed4233b342
jliriano <jd.liriano@alumnos.upm.es>
2018-10-14 17:07:57 +0200

    adding more tests and adding issue#06 branch to Travis - issue #06

--- src/test/java/api/ReviewsIT.java
+++ src/test/java/api/ReviewsIT.java
@@ -1,8 +1,16 @@
 package api;
 
+import api.apicontrollers.GameApiController;
+import api.apicontrollers.PublisherApiController;
+import api.apicontrollers.ReviewApiController;
 import api.daos.DaoFactory;
 import api.daos.memory.DaoMemoryFactory;
+import api.dtos.GameDto;
+import api.dtos.ReviewDto;
 import http.Client;
+import http.HttpException;
+import http.HttpRequest;
+import http.HttpStatus;
 import org.apache.logging.log4j.LogManager;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -17,13 +25,55 @@ public class ReviewsIT extends CommonCore {
     }
 
     @Test
-    void testCreateReview() {
+    void testCreateBasicReview() {
         String publisherId = this.createPublisher();
         LogManager.getLogger().info("publisherId: "+publisherId);
         String reviewId = this.createReview(publisherId, "This is my review message",
                 null, null, 0);
         assertNotNull(reviewId);
         LogManager.getLogger().info(new Client().submit(this.getPublisher(publisherId)).getBody());
+    }
+
+    @Test
+    void testCreateFullReview() {
+        String publisherId = this.createPublisher();
+        LogManager.getLogger().info("publisherId: "+publisherId);
+        String reviewId = this.createReview(publisherId, "This is my review message",
+                "My Review Title", "Author McPhee", 9);
+        assertNotNull(reviewId);
+        LogManager.getLogger().info(new Client().submit(this.getPublisher(publisherId)).getBody());
+    }
+
+    @Test
+    void testCreateReviewPublisherNotFoundException() {
+        ReviewDto reviewDto = new ReviewDto("Review message");
+        HttpRequest request = HttpRequest.builder(PublisherApiController.PUBLISHERS
+                +"/75"+ ReviewApiController.REVIEWS).body(reviewDto).post();
+        HttpException exception = assertThrows(HttpException.class, () -> new Client().submit(request));
+        assertEquals(HttpStatus.NOT_FOUND, exception.getHttpStatus());
+    }
+
+    @Test
+    void testCreateReviewInvalidReviewRating() {
+        String publisherId = this.createPublisher();
+        LogManager.getLogger().info("publisherId: "+publisherId);
+        ReviewDto reviewDto = new ReviewDto("Review message");
+        reviewDto.setReviewRating(19);
+        HttpRequest request = HttpRequest.builder(PublisherApiController.PUBLISHERS
+                +"/"+publisherId+ReviewApiController.REVIEWS).body(reviewDto).post();
+        HttpException exception = assertThrows(HttpException.class, () -> new Client().submit(request));
+        assertEquals(HttpStatus.BAD_REQUEST, exception.getHttpStatus());
+    }
+
+    @Test
+    void testCreateReviewInvalidReviewMessage() {
+        String publisherId = this.createPublisher();
+        LogManager.getLogger().info("publisherId: "+publisherId);
+        ReviewDto reviewDto = new ReviewDto(null);
+        HttpRequest request = HttpRequest.builder(PublisherApiController.PUBLISHERS
+                +"/"+publisherId+ReviewApiController.REVIEWS).body(reviewDto).post();
+        HttpException exception = assertThrows(HttpException.class, () -> new Client().submit(request));
+        assertEquals(HttpStatus.BAD_REQUEST, exception.getHttpStatus());
     }
 
 }
