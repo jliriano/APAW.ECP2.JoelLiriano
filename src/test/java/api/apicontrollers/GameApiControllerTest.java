ea13888b885d3fb4af252878669dcb2cf4b9af35
jliriano <jd.liriano@alumnos.upm.es>
2018-10-14 22:51:01 +0200

    add more test for coverage - issue #10

--- src/test/java/api/apicontrollers/GameApiControllerTest.java
+++ src/test/java/api/apicontrollers/GameApiControllerTest.java
@@ -6,10 +6,13 @@ import api.daos.DaoFactory;
 import api.daos.memory.DaoMemoryFactory;
 import api.dtos.GameDto;
 import api.dtos.PublisherDto;
+import api.entities.GameRating;
 import api.exceptions.ArgumentNotValidException;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -55,6 +58,16 @@ public class GameApiControllerTest {
     @Test
     void testIsNotValidCategory() {
         assertEquals(false, gameApiController.isValidCategory("NOPE"));
+    }
+
+    @Test
+    void testFullDtoCreate() {
+        publisherId = publisherBusinessController.create(new PublisherDto("Publisher"));
+        gameDto = new GameDto("Game 1", publisherId);
+        gameDto.setGameRating(GameRating.EVERYONE.name());
+        gameDto.setLaunchDate(LocalDateTime.now());
+        String gameId = gameBusinessController.create(gameDto);
+        assertNotNull(gameId);
     }
 
 }
