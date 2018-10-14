28dddc411b59f60c4952755575d61c673f82a4b7
jliriano <jd.liriano@alumnos.upm.es>
2018-10-14 16:23:22 +0200

    adding ReviewBusinessController - issue #06

---
+++ src/main/java/api/businesscontrollers/ReviewBusinessController.java
@@ -0,0 +1,35 @@
+package api.businesscontrollers;
+
+import api.daos.DaoFactory;
+import api.dtos.ReviewDto;
+import api.entities.Publisher;
+import api.entities.Review;
+
+import java.time.LocalDateTime;
+
+public class ReviewBusinessController {
+
+    private PublisherBusinessController publisherBusinessController = new PublisherBusinessController();
+
+    public String create(ReviewDto reviewDto, String publisherId) {
+        Publisher publisher = publisherBusinessController.getPublisher(publisherId);
+        this.processReviewDto(reviewDto);
+        Review review = new Review(reviewDto.getReviewMessage(), reviewDto.getTitle(),
+                reviewDto.getAuthor(), reviewDto.getReviewRating(), reviewDto.isPendingApproval());
+        DaoFactory.getFactory().getReviewDao().save(review);
+        publisher.addReview(review.getId());
+        DaoFactory.getFactory().getPublisherDao().save(publisher);
+        return review.getId();
+    }
+
+    private void processReviewDto(ReviewDto reviewDto) {
+        if(reviewDto.getAuthor()==null) {
+            reviewDto.setAuthor("Anonymous");
+        } else if(reviewDto.getTitle()==null) {
+            reviewDto.setTitle("Untitled");
+        } else if(reviewDto.getPublishedDate()==null) {
+            reviewDto.setPublishedDate(LocalDateTime.now());
+        }
+    }
+
+}
